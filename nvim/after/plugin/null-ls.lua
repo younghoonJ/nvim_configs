e1ea6817e1dd96510a3cb0f4a910538106115648
Younghoon Jung <69005067+younghoonJ@users.noreply.github.com>
2022-12-30 13:31:55 +0900

    Create null-ls.lua

---
+++ nvim/after/plugin/null-ls.lua
@@ -0,0 +1,18 @@
+local status, null_ls = pcall(require, "null-ls") 
+if not status then
+    return
+end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+
+null_ls.setup({
+    sources = {
+        formatting.stylua,
+        formatting.black.with({
+            extra_args = {"--fast"}
+        }),
+    },
+
+})
