383524de8ece7f0a61583d9ca7a0d9fac2c4e6f0
Younghoon Jung <69005067+younghoonJ@users.noreply.github.com>
2022-12-31 23:29:06 +0900

    Update indent-blankline.lua

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -3,6 +3,8 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
     show_end_of_line = true,
     space_char_blankline = " ",
 }
