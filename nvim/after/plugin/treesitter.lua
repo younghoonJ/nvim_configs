9d00e944138d83ad541995193c9ceb05778d54eb
Younghoon Jung <69005067+younghoonJ@users.noreply.github.com>
2023-01-06 00:59:09 +0900

    Update treesitter.lua

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -19,4 +19,5 @@ ensure_installed = { "help", "python", "c", "cpp", "lua", "rust" },
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  rainbow = { enable = true }
 }
