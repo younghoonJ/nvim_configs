2b6f5a623fb343080c86038ee36be409f096d8ac
Younghoon Jung <69005067+younghoonJ@users.noreply.github.com>
2023-01-30 19:49:19 +0900

    Update nvim-tree.lua

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -7,7 +7,35 @@ end
 vim.g.loaded = 1
 vim.g.loaded_newrwPlugin = 1
 
-vim.cmd([[highlight NvimTreeIndentMarker guifg=#3FC5FF]])
+
+local function open_nvim_tree(data)
+
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+        return
+    end
+
+    -- create a new, empty buffer
+    vim.cmd.enew()
+
+    -- wipe the directory buffer
+    vim.cmd.bw(data.buf)
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+
+
+-- vim.cmd([[highlight NvimTreeIndentMarker guifg=#3FC5FF]])
+
 
 nvimtree.setup({
     actions = {
