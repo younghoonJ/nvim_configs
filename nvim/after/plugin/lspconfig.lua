68aeee3be11e2913cda90b30188cf0a74d71e5af
younghoonJ <to.younghoon.jung@gmail.com>
2023-02-04 02:45:17 +0900

    remap changes

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -1,58 +1,13 @@
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = {
-	noremap = true,
-	silent = true,
-}
+local servers = { "pyright", "rust_analyzer", "clangd", "cmake", "sumneko_lua" }
 
--- Idea-like Mappings
-vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, { desc = "Format Document" })
-vim.keymap.set("n", "<F2>", vim.diagnostic.goto_next, opts, { desc = "Next diagnostic" })
 
--- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
--- vim.keymap.set('n', '<F2>', vim.diagnostic.goto_prev, opts)
--- vim.keymap.set("n", "<F2>", vim.diagnostic.goto_next, opts)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-local navic = require("nvim-navic")
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = {
-		noremap = true,
-		silent = true,
-		buffer = bufnr,
-	}
-
-	-- Idea-like Mappings
-	vim.keymap.set("n", "<C-b>", vim.lsp.buf.definition, bufopts, { desc = "[G]oto [D]efinition" })
-	vim.keymap.set("n", "<leader><F6>", vim.lsp.buf.rename, bufopts, { desc = "[R]e[n]ame" })
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts, { desc = "[R]e[n]ame" })
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts, { desc = "[C]ode [A]ction" })
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts, { desc = "[G]oto [D]efinition" })
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts, { desc = "[G]oto [I]mplementation" })
-	vim.keymap.set("n", "fr", vim.lsp.buf.references, bufopts, { desc = "[F]ind [R]eferences" })
-
-	-- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-
-	-- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	-- vim.keymap.set('n', '<space>wl', function()
-	--     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	-- end, bufopts)
-	-- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-
-	-- vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-
-	if client.server_capabilities.documentSymbolProvider then
-		navic.attach(client, bufnr)
-	end
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	require("younghoon.remap").attach(client, bufnr)
+    if client.server_capabilities.documentSymbolProvider then
+		require("nvim-navic").attach(client, bufnr)
+    end
 end
 
 -- nvim-cmp
@@ -60,46 +15,46 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- @see https://github.com/wookayin/dotfiles/blob/master/nvim/lua/config/lsp.lua
 do
-	-- :help lsp-method
-	-- :help lsp-handler
-	-- :help lsp-handler-configuration
-	local lsp_handlers_hover = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "single",
-	})
-	vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
-		local bufnr, winnr = lsp_handlers_hover(err, result, ctx, config)
-		if winnr ~= nil then
-			-- opacity/alpha for hover window
-			vim.api.nvim_win_set_option(winnr, "winblend", 30)
-		end
-		return bufnr, winnr
-	end
+    -- :help lsp-method
+    -- :help lsp-handler
+    -- :help lsp-handler-configuration
+    local lsp_handlers_hover = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "single",
+    })
+    vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
+        local bufnr, winnr = lsp_handlers_hover(err, result, ctx, config)
+        if winnr ~= nil then
+            -- opacity/alpha for hover window
+            vim.api.nvim_win_set_option(winnr, "winblend", 30)
+        end
+        return bufnr, winnr
+    end
 end
 
 do
-	vim.fn.sign_define("DiagnosticSignError", {
-		text = "",
-		texthl = "DiagnosticSignError",
-	})
-	vim.fn.sign_define("DiagnosticSignWarn", {
-		text = "",
-		texthl = "DiagnosticSignWarn",
-	})
-	vim.fn.sign_define("DiagnosticSignInfo", {
-		text = "",
-		texthl = "DiagnosticSignInfo",
-	})
-	vim.fn.sign_define("DiagnosticSignHint", {
-		text = "ﴞ",
-		texthl = "DiagnosticSignHint",
-	})
+    vim.fn.sign_define("DiagnosticSignError", {
+        text = "",
+        texthl = "DiagnosticSignError",
+    })
+    vim.fn.sign_define("DiagnosticSignWarn", {
+        text = "",
+        texthl = "DiagnosticSignWarn",
+    })
+    vim.fn.sign_define("DiagnosticSignInfo", {
+        text = "",
+        texthl = "DiagnosticSignInfo",
+    })
+    vim.fn.sign_define("DiagnosticSignHint", {
+        text = "ﴞ",
+        texthl = "DiagnosticSignHint",
+    })
 
-	-- vim.fn.sign_define("DiagnosticSignError",  {text = "✘", texthl = "DiagnosticSignError"})
-	-- vim.fn.sign_define("DiagnosticSignWarn",   {text = "", texthl = "DiagnosticSignWarn"})
-	-- vim.fn.sign_define("DiagnosticSignInfo",   {text = "i", texthl = "DiagnosticSignInfo"})
-	-- vim.fn.sign_define("DiagnosticSignHint",   {text = "", texthl = "DiagnosticSignHint"})
+    -- vim.fn.sign_define("DiagnosticSignError",  {text = "✘", texthl = "DiagnosticSignError"})
+    -- vim.fn.sign_define("DiagnosticSignWarn",   {text = "", texthl = "DiagnosticSignWarn"})
+    -- vim.fn.sign_define("DiagnosticSignInfo",   {text = "i", texthl = "DiagnosticSignInfo"})
+    -- vim.fn.sign_define("DiagnosticSignHint",   {text = "", texthl = "DiagnosticSignHint"})
 
-	vim.cmd([[
+    vim.cmd([[
         hi DiagnosticSignError    guifg=#e6645f ctermfg=167
         hi DiagnosticSignWarn     guifg=#b1b14d ctermfg=143
         hi DiagnosticSignHint     guifg=#3e6e9e ctermfg=75
@@ -107,66 +62,66 @@ do
 end
 
 if vim.diagnostic then
-	-- Customize how to show diagnostics:
-	-- @see https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
-	-- @see https://github.com/neovim/neovim/pull/16057 for new APIs
-	vim.diagnostic.config({
-		-- No virtual text (distracting!), show popup window on hover.
-		virtual_text = false,
-		signs = true,
-		underline = {
-			-- Do not underline text when severity is low (INFO or HINT).
-			severity = {
-				min = vim.diagnostic.severity.WARN,
-			},
-		},
-		float = {
-			source = "always",
-			focusable = true,
-			focus = false,
-			border = "single",
+    -- Customize how to show diagnostics:
+    -- @see https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
+    -- @see https://github.com/neovim/neovim/pull/16057 for new APIs
+    vim.diagnostic.config({
+        -- No virtual text (distracting!), show popup window on hover.
+        virtual_text = false,
+        signs = true,
+        underline = {
+            -- Do not underline text when severity is low (INFO or HINT).
+            severity = {
+                min = vim.diagnostic.severity.WARN,
+            },
+        },
+        float = {
+            source = "always",
+            focusable = true,
+            focus = false,
+            border = "single",
 
-			-- Customize how diagnostic message will be shown: show error code.
-			format = function(diagnostic)
-				-- See null-ls.nvim#632, neovim#17222 for how to pick up `code`
-				local user_data
-				user_data = diagnostic.user_data or {}
-				user_data = user_data.lsp or user_data.null_ls or user_data
-				-- TODO: symbol is specific to pylint (will be removed)
-				local code = (diagnostic.symbol or diagnostic.code or user_data.symbol or user_data.code)
-				if code then
-					return string.format("%s (%s)", diagnostic.message, code)
-				else
-					return diagnostic.message
-				end
-			end,
-		},
-	})
-	LspDiagnosticsShowPopup = function()
-		return vim.diagnostic.open_float(0, {
-			scope = "cursor",
-		})
-	end
+            -- Customize how diagnostic message will be shown: show error code.
+            format = function(diagnostic)
+                -- See null-ls.nvim#632, neovim#17222 for how to pick up `code`
+                local user_data
+                user_data = diagnostic.user_data or {}
+                user_data = user_data.lsp or user_data.null_ls or user_data
+                -- TODO: symbol is specific to pylint (will be removed)
+                local code = (diagnostic.symbol or diagnostic.code or user_data.symbol or user_data.code)
+                if code then
+                    return string.format("%s (%s)", diagnostic.message, code)
+                else
+                    return diagnostic.message
+                end
+            end,
+        },
+    })
+    LspDiagnosticsShowPopup = function()
+        return vim.diagnostic.open_float(0, {
+            scope = "cursor",
+        })
+    end
 end
 
 -- Show diagnostics in a pop-up window on hover
 do
-	LspDiagnosticsPopupHandler = function()
-		local current_cursor = vim.api.nvim_win_get_cursor(0)
-		local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
+    LspDiagnosticsPopupHandler = function()
+        local current_cursor = vim.api.nvim_win_get_cursor(0)
+        local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
 
-		-- Show the popup diagnostics window,
-		-- but only once for the current cursor location (unless moved afterwards).
-		if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
-			vim.w.lsp_diagnostics_last_cursor = current_cursor
-			local _, winnr = LspDiagnosticsShowPopup()
-			if winnr ~= nil then
-				-- opacity/alpha for diagnostics
-				vim.api.nvim_win_set_option(winnr, "winblend", 30)
-			end
-		end
-	end
-	vim.cmd([[
+        -- Show the popup diagnostics window,
+        -- but only once for the current cursor location (unless moved afterwards).
+        if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+            vim.w.lsp_diagnostics_last_cursor = current_cursor
+            local _, winnr = LspDiagnosticsShowPopup()
+            if winnr ~= nil then
+                -- opacity/alpha for diagnostics
+                vim.api.nvim_win_set_option(winnr, "winblend", 30)
+            end
+        end
+    end
+    vim.cmd([[
         augroup LSPDiagnosticsOnHover
         autocmd!
         autocmd CursorHold *   lua LspDiagnosticsPopupHandler()
@@ -175,46 +130,44 @@ do
 end
 
 local lsp_flags = {
-	-- This is the default in Nvim 0.7+
-	debounce_text_changes = 150,
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
 }
 
 -- nice-reference
 
 require("nice-reference").setup({
-	anchor = "NW", -- Popup position anchor
-	relative = "cursor", -- Popup relative position
-	row = 1, -- Popup x position
-	col = 0, -- Popup y position
-	border = "rounded", -- Popup borderstyle
-	winblend = 0, -- Popup transaparency 0-100, where 100 is transparent
-	max_width = 120, -- Max width of the popup
-	max_height = 10, -- Max height of the popup
-	auto_choose = false, -- Go to reference if there is only one
+    anchor = "NW", -- Popup position anchor
+    relative = "cursor", -- Popup relative position
+    row = 1, -- Popup x position
+    col = 0, -- Popup y position
+    border = "rounded", -- Popup borderstyle
+    winblend = 0, -- Popup transaparency 0-100, where 100 is transparent
+    max_width = 120, -- Max width of the popup
+    max_height = 10, -- Max height of the popup
+    auto_choose = false, -- Go to reference if there is only one
 })
 
-local servers = { "pyright", "rust_analyzer", "clangd", "cmake", "sumneko_lua" }
 for _, lsp in pairs(servers) do
-	require("lspconfig")[lsp].setup({
-		on_attach = on_attach,
-		flags = lsp_flags,
-		capabilities = capabilities,
-	})
+    require("lspconfig")[lsp].setup({
+        on_attach = on_attach,
+        flags = lsp_flags,
+        capabilities = capabilities,
+    })
 end
 
--- which-key
-
 vim.o.timeout = true
+
 vim.o.timeoutlen = 300
 
 require("which-key").setup({
-	conf = { window = { border = "single", position = "bottom" } },
-	-- opts = {
-	-- 	mode = "n", -- Normal mode
-	-- 	prefix = "<leader>",
-	-- 	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	-- 	silent = true, -- use `silent` when creating keymaps
-	-- 	noremap = true, -- use `noremap` when creating keymaps
-	-- 	nowait = false, -- use `nowait` when creating keymaps
-	-- },
+    conf = { window = { border = "single", position = "bottom" } },
+    -- opts = {
+    -- 	mode = "n", -- Normal mode
+    -- 	prefix = "<leader>",
+    -- 	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    -- 	silent = true, -- use `silent` when creating keymaps
+    -- 	noremap = true, -- use `noremap` when creating keymaps
+    -- 	nowait = false, -- use `nowait` when creating keymaps
+    -- },
 })
--- nvim/lua/younghoon/remap.lua
+++ nvim/lua/younghoon/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "exit to the file explorer" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>yv=gv", { desc = "move visual block" })
@@ -13,18 +14,82 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to the system clipboard" })
 
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "[S]plit window [V]ertically" })
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "[S]plit window [H]orizontally" })
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "[S]plit windows [E]qual width" })
-vim.keymap.set("n", "<leader>sc", ":close<CR>", { desc = "[S]plit window [C]lose" })
 
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = "[T]ab [O]pen" })
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = "[T]ab [C]lose" })
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = "[T]ab [N]ext" })
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = "[T]ab [P]rev" })
-vim.keymap.set("n", "<C-t>", ":tabn<CR>", { desc = "[T]ab [N]ext" })
+local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<A-2>", ":BufferNext<CR>", { desc = "Buffer Next" })
-vim.keymap.set("n", "<A-4>", ":BufferDelete<CR>", { desc = "Buffer Delete" })
-vim.keymap.set("n", "<A-3>", "<Cmd>lua require('younghoon.utils').buff_selector() <CR>", {desc = "BufferPick"})
+local kmap_buffer = {
+    name = "buffer",
+    ["<A-2>"] = {":BufferNext<CR>", "Buffer Next" },
+    ["<A-3>"] = {"<Cmd>lua require('younghoon.utils').buff_selector() <CR>", "BufferPick"},
+    ["<A-4>"] = {":BufferDelete<CR>","Buffer Delete"},
+}
 
+local kmap_window = {
+    name = "window",
+    ["<leader>sv"] = {"<C-w>v",  "[S]plit window [V]ertically" },
+    ["<leader>sh"] = {"<C-w>s",  "[S]plit window [H]orizontally" },
+    ["<leader>se"] = {"<C-w>=",  "[S]plit windows [E]qual width" },
+    ["<leader>sc"] = {":close<CR>",  "[S]plit window [C]lose" },
+}
+
+local kmap_tab = {
+    ["<leader>to"] = {":tabnew<CR>",   "[T]ab [O]pen" },
+    ["<leader>tx"] = {":tabclose<CR>", "[T]ab [C]lose" },
+    ["<leader>tn"] = {":tabn<CR>",     "[T]ab [N]ext" },
+    ["<leader>tp"] = {":tabp<CR>",     "[T]ab [P]rev" },
+    ["<C-t>"] = { ":tabn<CR>",  "[T]ab [N]ext" },
+}
+
+local wk = require "which-key"
+wk.register(kmap_buffer, opts)
+wk.register(kmap_window, opts)
+wk.register(kmap_buffer, opts)
+
+local M = {}
+
+local function __maps__(client, bufnr)
+    
+    -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+    -- vim.keymap.set('n', '<F2>', vim.diagnostic.goto_prev, opts)
+    -- vim.keymap.set("n", "<F2>", vim.diagnostic.goto_next, opts)
+    -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+
+    -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    -- vim.keymap.set('n', '<space>wl', function()
+    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, bufopts)
+    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+
+    local kmap_lsp = {
+        ["<F2>"] =  {"<cmd>lua vim.diagnostic.goto_next()<CR>", "Next diagnostic" },
+        ["<leader>"] = {
+            ["<F6>"] = {"<cmd>lua vim.lsp.buf.rename()<CR>", "[R]e[n]ame" },
+            ["rn"] = {"<cmd>lua vim.lsp.buf.rename()<CR>", "[R]e[n]ame" },
+            ["ca"] = {"<cmd>lua vim.lsp.buf.code_action()<CR>", "[C]ode [A]ction"},
+        },
+        g = {
+            name = "GoTo",
+            d = {"<cmd>lua vim.lsp.buf.definition()<CR>", "[G]oto [D]efinition"},
+            i = {"<cmd>lua vim.lsp.buf.implementation()<CR>", "[G]oto [I]mplementation"},
+            r = {"<cmd>lua vim.lsp.buf.references()<CR>", "[G]oto [R]eferences"},
+        },
+        ["<C-l>"] = {"<cmd>lua vim.lsp.buf.format()<CR>",  "Format Document" },
+        ["<C-b>"] = {"<cmd>lua vim.lsp.buf.definition()<CR>", "[G]oto [D]efinition" },
+        
+    }
+
+
+    local bufopts = {noremap = true, silent = true, buffer = bufnr,}
+    
+    wk.register(kmap_lsp, bufopts)
+
+end
+
+function M.attach(client, bufnr)
+    __maps__(client, bufnr)
+end
+
+return M
